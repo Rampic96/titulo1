60e409b6870ffc4031741514c0bb087c10a04288
Rampic96 <44008051+Rampic96@users.noreply.github.com>
2021-09-17 15:51:19 -0300

    Nuevo repositorio

---
+++ Prueba.py
@@ -0,0 +1,7 @@
+import random
+from time import sleep
+from selenium  import webdriver
+
+driver = webdriver.Chrome(executable_path=r'C:\Chrome\chromedriver.exe') # to open the chromebrowser 
+driver.get('https://www.falabella.com/falabella-cl/category/cat2028/Notebooks-Gamers')
+
